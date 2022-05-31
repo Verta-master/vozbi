c30d049825eef7d37f37fb41ae5a674b71832478
Verta-master <vertyagina@gmail.com>
2022-05-31 16:59:37 +0600

    markup

---
+++ js/catalog.js
@@ -0,0 +1,34 @@
+function postsCarousel() {
+  var checkWidth = $(window).width();
+  var owlPost = $(".category__slider");
+  
+  if (checkWidth > 1200) {
+    if (typeof owlPost.data('owl.carousel') != 'undefined') {
+      owlPost.data('owl.carousel').destroy();
+    }
+    owlPost.removeClass('owl-carousel');
+  } else if (checkWidth < 1201) {
+    owlPost.addClass('owl-carousel');
+    owlPost.owlCarousel({
+      loop: true,
+      margin: 30,
+      nav: true,
+      dots: false,
+      navText:["",""],
+      responsive:{
+        0:{
+          items: 1
+        },
+        480:{
+          items: 2,
+        },
+        766:{
+          items: 3,
+        },
+      }
+    });
+  }
+}
+
+postsCarousel();
+$(window).resize(postsCarousel);	
