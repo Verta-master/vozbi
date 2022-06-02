4a444470690e7bbcd1ee954dfbc795c481cb11fe
Verta-master <vertyagina@gmail.com>
2022-06-02 16:31:18 +0600

    adaptive

--- js/range-slider.js
+++ js/range-slider.js
@@ -28,3 +28,22 @@ $( function() {
   $( "#amount-min2" ).val( $( "#slider-range2" ).slider( "values", 0 ) + " м2" );
   $( "#amount-max2" ).val( $( "#slider-range2" ).slider( "values", 1 ) + " м2" );
 });
+
+//mobile filter
+if (window.innerWidth < 1260) {
+  $('.filter__title').click(function() {
+    event.preventDefault();
+    $('.filter__field').addClass('filter__field--open');
+    $('.filter__options').scroll(function(){
+      $('.filter__options').getNiceScroll().resize();
+    });
+  })
+  
+  $('.filter__close').click(function() {
+    $('.filter__field').removeClass('filter__field--open');
+  })
+  
+  $('.filter__options').niceScroll({
+    cursorcolor: '#5D3589',
+  });
+}
